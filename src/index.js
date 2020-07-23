dc8f35623eef6d77fb78df3efbb5851580439d2d
Nikita Voronov <amensum@ya.ru>
2020-07-23 17:40:13 +0300

    [UP]

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/** @format */
+
+import React, { useEffect, useMemo, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const usePermanentState = (initState) => {
+  const [state, setState] = React.useState(initState);
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem('@state').then((json) => {
+      const state = JSON.parse(json);
+
+      if (state) {
+        setState(state);
+      }
+
+      setMounted(true);
+
+      console.log('[PS] STORAGE ⇨ STATE');
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (mounted) {
+      const json = JSON.stringify(state);
+
+      AsyncStorage.setItem('@state', json).then(() => {
+        console.log('[PS] STATE ⇨ STORAGE');
+      });
+    }
+  }, [state]);
+
+  const unsetState = () => {
+    setState(initState);
+
+    console.log('[PS] ✕ STORAGE');
+  };
+
+  return [state, setState, unsetState];
+};
+
+module.exports = {
+  usePermanentState,
+};
